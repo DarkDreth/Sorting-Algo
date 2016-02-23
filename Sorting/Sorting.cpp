2aad2e8d3a79aba8b6fd7d6bcff2044b5ce12c63
DarkDreth <jordoncain@mail.usf.edu>
2016-02-23 00:49:54 -0500

    Insertion sort addd

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -29,5 +29,17 @@ template <class T> void selectionsort(T* data, int size)
 
 template <class T> void InsertionSort(T* data, int size)
 {
-
+	T temp;
+	for (int i = 1; i < size; i++)
+	{
+		for (int j = i - 1; i > 0; i--)
+		{
+			if (*(data + i) < *(data + j))
+			{
+				temp = *(data + i);
+				*(data + i) = *(data + j);
+				*(data + j) = temp;
+			}
+		}
+	}
 }
