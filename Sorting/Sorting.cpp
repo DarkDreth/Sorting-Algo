688d4ef2e6af677367291bc24051e85adf029516
DarkDreth <jordoncain@mail.usf.edu>
2016-02-23 02:15:45 -0500

    Finished mergesort, working on Quicksort.

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -27,7 +27,11 @@ template <class T> void selectionsort(T* data, int size)
 	}
 }
 
-template <class T> void InsertionSort(T* data, int size)
+/*
+* insertionsort sorts an array of data
+* expected O(n^2)
+*/
+template <class T> void insertionsort(T* data, int size)
 {
 	T temp;
 	for (int i = 1; i < size; i++)
@@ -41,5 +45,63 @@ template <class T> void InsertionSort(T* data, int size)
 				*(data + j) = temp;
 			}
 		}
+	}
+}
+
+/*
+* mergesort sorts an array of data
+* expected O(nlogn)
+*/
+template <class T> void mergesort(T* data, int size, T* temp)
+{
+	if (size > 1)
+	{
+		int mid = (n + 1) / 2;
+		T* right = (data + mid);
+
+		//left array to sort
+		mergesort(data, mid - 1, temp);
+		mergesort(data, size - 1, right);
+		int l = r = s = 0; //TODO: Check this
+		while (l < mid && r < size - mid)
+		{
+			if (*(data + l) < *(right + r))
+			{
+				*(temp + s) = *(data + l);
+				l++;
+			}
+
+			else
+			{
+				temp[s] = *(right + r);
+				r++;
+			}
+			
+			s++;
+		}
+		mergecopy(data, mid - 1, temp);
+		mergecopy(right, size, temp);
+		mergecopy(temp, size, data);
+	}
+}
+
+template <class T> void quicksort(T* data, int size)
+{
+	if (size < 1)
+	{
+		return data;
+	}
+	int mid = (size + 1) / 2;
+
+}
+
+/*
+* Copies on array into another
+*/
+template <class T> void mergecopy(T* data, int size, T* temp)
+{
+	for (i = 0; i < size; i++)
+	{
+		*(temp + i) = *(data + i)
 	}
 }
